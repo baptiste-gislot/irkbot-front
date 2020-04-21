91732f9f249342419f64e5f680db203b09a77f16
kobesan <baptiste.gislot@gmail.com>
2020-04-21 12:04:38 +0200

    pagination

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,32 +1,83 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Table, Button } from "react-bootstrap";
 import Question from "./Question";
 
 const apiUrl = "http://127.0.0.1";
 const apiPort = "8000";
-const start = 0;
-const gap = 15;
+
+const buttonStyle = {
+  margin: "5px",
+};
 
 const Dashboard = () => {
+  const [start, setStart] = useState(0);
+  const [gap, setGap] = useState(15);
   const [questions, setQuestions] = useState([]);
 
-  useEffect(() => {
+  const getData = (pagination) => {
     axios
-      .get(`${apiUrl}:${apiPort}/questions?start=${start}&gap=${gap}`)
+      .get(`${apiUrl}:${apiPort}/questions?start=${pagination}&gap=${gap}`)
       .then((res, err) => {
         if (err) {
           console.log(err);
         }
         setQuestions(res.data);
       });
+  };
+
+  const handlePrevious = () => {
+    if (start >= 15) {
+      setStart(start - gap);
+    } else {
+      setStart(0);
+    }
+    getData(start - gap);
+  };
+
+  const handleNext = () => {
+    setStart(start + gap);
+    getData(start + gap);
+  };
+
+  useEffect(() => {
+    getData(0);
   }, []);
 
   return (
     <React.Fragment>
       <h1>Dashboard</h1>
-      {questions.map((question) => {
-        return <Question question={question} />;
-      })}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Topic</th>
+            <th>Question</th>
+            <th>Answer</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {questions.map((question, index) => {
+            return (
+              <Question
+                question={question}
+                key={index}
+                index={index}
+                setData={() => setQuestions()}
+              />
+            );
+          })}
+        </tbody>
+      </Table>
+      <div>
+        <Button variant="primary" style={buttonStyle} onClick={handlePrevious}>
+          Previous
+        </Button>
+        <Button variant="primary" style={buttonStyle} onClick={handleNext}>
+          Next
+        </Button>
+      </div>
     </React.Fragment>
   );
 };
--- src/Components/Question.jsx
+++ src/Components/Question.jsx
@@ -1,13 +1,37 @@
 import React from "react";
+import axios from "axios";
+
+const imgStyle = {
+  width: "20px",
+  margin: "0 5px",
+  cursor: "pointer",
+};
 
 const Question = (props) => {
+  const deleteQ = (id) => {
+    axios.delete(`http://127.0.0.1:8000/questions/${id}`).then((res, err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  };
+
   return (
-    <React.Fragment>
-      <p>{props.question.id}</p>
-      <p>{props.question.topic}</p>
-      <p>{props.question.question}</p>
-      <p>{props.question.answer}</p>
-    </React.Fragment>
+    <tr>
+      <td>{props.question.id}</td>
+      <td>{props.question.topic}</td>
+      <td>{props.question.question}</td>
+      <td>{props.question.answer}</td>
+      <td>
+        <img src="/edit.svg" style={imgStyle} alt="edit"></img>
+        <img
+          src="/delete.svg"
+          style={imgStyle}
+          alt="delete"
+          onClick={() => deleteQ(props.index)}
+        ></img>
+      </td>
+    </tr>
   );
 };
 
